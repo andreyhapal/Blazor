c60af1f4ee916db3f0c49f834fbe2556e74fec2f
a.khapal <a.khapal@linker.by>
2021-02-22 15:57:03 +0300

    add profile and fix weather forecast dialog

---
+++ BlazorExample/Data/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExample.Data
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Speciality { get; set; }
+        public string Phone { get; set; }
+        public string Img { get; set; }
+    }
+}
